e195ead31adbd99aaea9ef5cfdf2cb0e8ef91b31
Gieandes Silva <contato@gieandessilva.com>
2017-10-15 23:07:11 -0200

    FrontEnd Implementado

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Pagination\Paginator;
+
+use App\Product;
+
+class FrontEndController extends Controller
+{
+    //
+
+    public function index()
+    
+    {
+
+        $produtos = Product::orderBy('created_at', 'desc')->paginate(5);
+
+        return view('index', ['products' => $produtos]);
+    }
+}
